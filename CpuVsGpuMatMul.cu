957a481d7b0b24a9a9b7e7816134dd130705cb98
ThatBananer <30753385+Jon-1162@users.noreply.github.com>
2025-06-30 19:36:26 -0400

    Update CpuVsGpuMatMul.cu

--- CpuVsGpuMatMul.cu
+++ CpuVsGpuMatMul.cu
@@ -2,29 +2,36 @@
 #include <nvtx3/nvToolsExt.h>
 #include <iostream>
 
-5 3 7
-8 2 9
 
-int[] cpuMatMul( int matrix1colCount, int matrix1rowCount, int[] matrix1, int matrix2colCount, int matrix2rowCount, int[] matrix2) {
-    int returnArr[matrix2colCount];
-    // for loop of column count of matrix 2 
-    for (int i = 0; i < matrix2columnCount){
-        int sum  = 0
-        // for loop on column count of matrix 1
-        for (int j = 0; j < matrix1colCount; j++ ){
-            int product = matrix1[j]*matrix2[j*matrix2colCount];
-            sum += product;
-        }
-        returnArr[i] = sum;
-        
+// #1      #2
+//  1 3    2 0
+//  4 6    5 4
+
+int[][] cpuMatMul( int &matrix1colCount, int &matrix1rowCount, int[][] &matrix1, int &matrix2colCount, int &matrix2rowCount, int[][] &matrix2) {
+    int returnArr[matrix1colCount][matrix2rowCount];
+    // 
+    for (int i = 0; i < matrix1RowCount){
+        int sum = 0;
+        // 
+        for (int j = 0; j < matrix2rowCount; j++ ){
+            returnArr[i][j] = 0;
+            // goes through matrix 2 columns
+            for (int k = 0; k < matrix2RowCount; k++) {
+                returnArr[i][j] = matrix1[i][k]*matrix2[k][j];
+            }
+        }       
     }
 
-    return returnArr
-
+    return returnArr;
 
 }
 
 int main() {
+    int matLen = 2
+    int[matLen][matLen] matrix1 = {{1,3},{4,6}};
+    int[matLen][matLen] matrix2 = {{2,0},{5,4}};
+    int[matLen][matLen] cpuMatMulOutput = cpuMatMul(2,2, matrix1, 2,2, matrix2);
+    
   
   return 0;
 }
