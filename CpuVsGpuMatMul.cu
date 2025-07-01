75f493936e60a2d84902f0199c9ad5d7c8293023
ThatBananer <30753385+Jon-1162@users.noreply.github.com>
2025-06-30 20:09:58 -0400

    Update CpuVsGpuMatMul.cu

--- CpuVsGpuMatMul.cu
+++ CpuVsGpuMatMul.cu
@@ -1,37 +1,46 @@
 #include <cuda_runtime.h>
 #include <nvtx3/nvToolsExt.h>
 #include <iostream>
+using namespace std;
+
 
 
 // #1      #2
 //  1 3    2 0
 //  4 6    5 4
 
-int[][] cpuMatMul( int &matrix1colCount, int &matrix1rowCount, int[][] &matrix1, int &matrix2colCount, int &matrix2rowCount, int[][] &matrix2) {
-    int returnArr[matrix1colCount][matrix2rowCount];
+// 15
+// 12
+// 30
+// 24
+void cpuMatMul( int matrix1ColCount, int matrix1RowCount, auto  &matrix1, int matrix2ColCount, int matrix2RowCount, auto &matrix2) {
+    int returnArr[matrix1ColCount][matrix2RowCount];
     // 
-    for (int i = 0; i < matrix1RowCount){
+    for (int i = 0; i < matrix1RowCount; i++){
         int sum = 0;
         // 
-        for (int j = 0; j < matrix2rowCount; j++ ){
+        for (int j = 0; j < matrix2RowCount; j++ ){
             returnArr[i][j] = 0;
             // goes through matrix 2 columns
             for (int k = 0; k < matrix2RowCount; k++) {
-                returnArr[i][j] = matrix1[i][k]*matrix2[k][j];
+                returnArr[i][j] += matrix1[i][k]*matrix2[k][j];
+                
             }
+            cout << returnArr[i][j] << endl;
         }       
     }
 
-    return returnArr;
+    // return returnArr;
 
 }
 
 int main() {
-    int matLen = 2
-    int[matLen][matLen] matrix1 = {{1,3},{4,6}};
-    int[matLen][matLen] matrix2 = {{2,0},{5,4}};
-    int[matLen][matLen] cpuMatMulOutput = cpuMatMul(2,2, matrix1, 2,2, matrix2);
+    int matLen = 2;
+    int matrix1[2][2] = {{1,3},{4,6}};
+    int matrix2[2][2] = {{2,0},{5,4}};
+    cpuMatMul(2,2, matrix1, 2,2, matrix2);
     
   
   return 0;
 }
+
