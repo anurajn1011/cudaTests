bbb77aa64ece34437b4c6cbbf4b4e1ef9995a2ff
ThatBananer <30753385+Jon-1162@users.noreply.github.com>
2025-07-14 19:21:48 -0400

    moved things+ added analysis

--- Modeling/Modeling.py
+++ Modeling/Modeling.py
@@ -4,6 +4,8 @@ import pandas as pd
 import csv
 from datetime import date, datetime, timedelta
 from statsmodels.tsa.stattools import coint, adfuller
+from statsmodels.regression.linear_model import GLS
+
 from typing import Tuple, Iterable
 
 # Alpaca imports
@@ -17,23 +19,38 @@ from dotenv import load_dotenv
 
 # AlphaVantage Imports
 from alpha_vantage.timeseries import TimeSeries
-
+from engle_granger import OLSResiduals
 import numpy as np
 import matplotlib.pyplot as plt
+from DataProcessing import DataProcessing
 
-from stochastic.processes.diffusion.vasicek import OrnsteinUhlenbeckProcess
-import matplotlib.pyplot as plt
+def main():
+    df = pd.read_csv(r'C:\Users\jco10\Documents\PersonalProjects\cudaWAnuraj\cudaTests\results\cointegration_pairs_sample.csv')
 
-# Create an OU process instance
-ou = OrnsteinUhlenbeckProcess(speed=0.7, vol=0.06, t=1)
+    
+    alpaca = DataProcessing()
+    start, end = alpaca.set_time(7, 3, 2025)
 
-# Sample the process (defaults to 1000 steps)
-sample_path = ou.sample()
+    
+    for key, value in df[['key', 'value']].to_numpy():
+        # Step 1: OLS on two cointegrated assets to obtain residuals Z_t    
+        print(key, value)
+        series1 = alpaca.get_symbol_history(key, start, end)
+        series2 = alpaca.get_symbol_history(value, start, end)
+        series1,series2 = alpaca.drop_rows(series1, series2)
+        residuals = OLSResiduals(series1=series1, series2=series2)  
+        # Step 2: Compute delta Z_t by taking the differences of every element with its previous one
+        deltaZ = residuals.diff().dropna()
+        # Step 3: Regress delta Zt on z(t-1) via GLS.
+        zLag = residuals.iloc[1:]
+        gls_model = GLS(deltaZ, zLag).fit()
+        print(gls_model.summary())
+        
 
-# Plot it
-plt.plot(sample_path)
-plt.title("Ornstein-Uhlenbeck Process")
-plt.xlabel("Time step")
-plt.ylabel("X_t")
-plt.show()
+    
+
+if __name__ == "__main__":
+    main()
+
+
 
