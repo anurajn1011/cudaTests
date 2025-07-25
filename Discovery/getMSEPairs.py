6100e9e2927ec6135b3ec6c01c1bff1cbf74d861
ThatBananer <30753385+Jon-1162@users.noreply.github.com>
2025-07-24 22:31:56 -0400

    finished discovery, adding results

--- Discovery/getMSEPairs.py
+++ Discovery/getMSEPairs.py
@@ -43,12 +43,12 @@ def filterStockForStationarity(df, output_file):
 def generateStockPair2CSV_MSE(df, output_file, threshold=5) -> None:
     data_batch = {}
     for i in range(len(df)):
-        if df.iloc[i, 1].isalpha():
-            series1 = alpaca.get_symbol_history(df.iloc[i, 1], start, end)
+        if df.iloc[i, 0].isalpha():
+            series1 = alpaca.get_symbol_history(df.iloc[i, 0], start, end)
         # type checks
         if isinstance(series1, pd.DataFrame):
             if 'open' not in series1.columns:
-                print(f"\t'open' column missing in series1 for {df.iloc[i, 1]}, skipping.")
+                print(f"\t'open' column missing in series1 for {df.iloc[i, 0]}, skipping.")
                 continue
             series1 = series1['open'].reset_index(level='symbol', drop=True)
         elif isinstance(series1, pd.Series):
@@ -59,15 +59,15 @@ def generateStockPair2CSV_MSE(df, output_file, threshold=5) -> None:
             continue
 
         for j in range(i + 1, len(df)):
-            if df.iloc[j, 1].isalpha():
-                series2 = alpaca.get_symbol_history(df.iloc[j, 1], start, end)
+            if df.iloc[j, 0].isalpha():
+                series2 = alpaca.get_symbol_history(df.iloc[j, 0], start, end)
                 if series2.empty:
-                    print(f"\tSeries2 for {df.iloc[j, 1]} is empty, skipping.")
+                    print(f"\tSeries2 for {df.iloc[j, 0]} is empty, skipping.")
                     continue  # skip to next j
                 # checks to ensure that data is handled correctly
                 if isinstance(series2, pd.DataFrame):
                     if 'open' not in series2.columns:
-                        print(f"\t'open' column missing in series2 for {df.iloc[j, 1]}, skipping.")
+                        print(f"\t'open' column missing in series2 for {df.iloc[j, 0]}, skipping.")
                         continue
                     series2 = series2['open'].reset_index(level='symbol', drop=True)
                 elif isinstance(series2, pd.Series):
@@ -78,21 +78,22 @@ def generateStockPair2CSV_MSE(df, output_file, threshold=5) -> None:
                     continue
                 if len(series1) <= 20 or len(series2) <= 20:
                     break
-                print(df.iloc[i, 1], df.iloc[j, 1])
+                print(df.iloc[i, 0], df.iloc[j, 0])
                 series1, series2 = series1.align(series2, join='inner')
                 mse = meanSquaredDistance(series1, series2)
                 if mse < threshold:
-                    print(f"MSE of {df.iloc[i, 1]} and {df.iloc[j, 1]} is: {mse}")
-                    data_batch[df.iloc[i, 1]] = (df.iloc[j, 1], mse)
-                    writer(output_file, data_batch)
-        
+                    print(f"MSE of {df.iloc[i, 0]} and {df.iloc[j, 0]} is: {mse}")
+                    data_batch[df.iloc[i, 0]] = (df.iloc[j, 0], mse)
+    writer(output_file, data_batch)
 
+        
 # 'C:\Users\User\Documents\Projects\cudaTests\results\nasdaq_screener_MSE.csv'
 def writer(filename, data_batch) -> None:
-     with open(filename, "a", newline="") as f:
+     with open(filename, "w", newline="") as f:
         w = csv.writer(f)
+        w.writerow(["stock1","stock2","mse"])
         for index, (key, value) in enumerate(data_batch.items()):
-            w.writerow([index, key] + list(value))
+            w.writerow([index, key, value]  )
 
 def main():
     data_batch = {}
--- Discovery/main.py
+++ Discovery/main.py
@@ -1,24 +1,26 @@
 import pandas as pd
 import datetime
-from GetMSEPairs import filterStockForStationarity, generateStockPair2CSV_MSE
+from getMSEPairs import filterStockForStationarity, generateStockPair2CSV_MSE
 from CoIntegrationTestLoop import testStockPairForCoInt
 
 def main():
-    start = datetime(2024, 1, 1, 13, 0) 
-    end = datetime(2025, 1, 1, 13, 0) 
+    # start = datetime(2024, 1, 1, 13, 0) 
+    # end = datetime(2025, 1, 1, 13, 0) 
     
     # check for stationarity
-    an_df_raw = pd.read_csv(r'C:\Users\User\Documents\Projects\cudaTests\datasets\nasdaq_screener.csv')
-    an_stationarityFilter = r"C:\Users\User\Documents\Projects\cudaTests\CleanedData\nasdaq_screener_filtered_for_stationarity.csv"
-    filterStockForStationarity(an_df_raw, an_stationarityFilter)
+    # an_df_raw = pd.read_csv(r'C:\Users\User\Documents\Projects\cudaTests\datasets\nasdaq_screener.csv')
+    # an_stationarityFilter = r"C:\Users\User\Documents\Projects\cudaTests\CleanedData\nasdaq_screener_filtered_for_stationarity.csv"
+    jc_stationarityFilter = r"C:\Users\jco10\Documents\PersonalProjects\cudaWAnuraj\cudaTests\CleanedData\nasdaq_screener_filtered_for_stationarity.csv"
+    # filterStockForStationarity(an_df_raw, an_stationarityFilter)
 
     # check pairs that satisfy MSE constraints
-    an_df_cleaned = pd.read_csv(an_stationarityFilter)
-    an_MSE = r"C:\Users\User\Documents\Projects\cudaTests\results\cleaned_nasdaq_screener_MSE.csv"
-    generateStockPair2CSV_MSE(an_df_cleaned, an_MSE)
+    df_cleaned = pd.read_csv(jc_stationarityFilter)
+    # an_MSE = r"C:\Users\User\Documents\Projects\cudaTests\results\cleaned_nasdaq_screener_MSE.csv"
+    jc_MSE = r"C:\Users\jco10\Documents\PersonalProjects\cudaWAnuraj\cudaTests\results\cleaned_nasdaq_screener_MSE.csv"
+    generateStockPair2CSV_MSE(df_cleaned, jc_MSE)
 
     # run the cointegration testloop
-    testStockPairForCoInt(an_MSE, start, end)
+    # testStockPairForCoInt(an_MSE, start, end)
 
 if __name__ == "__main__":
     main()
